9500228718e6e864f230cfd8dc7db643878a7423
DM Huyền <dominhhuyen2303@gmail.com>
2024-08-11 02:48:58 +0700

    update

--- src/test/java/BT2/AddCategory.java
+++ src/test/java/BT2/AddCategory.java
@@ -1,6 +1,7 @@
 package BT2;
 
 import common.BaseTest;
+import keywords.WebUI;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
@@ -13,13 +14,14 @@ public class AddCategory extends BaseTest {
         driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@example.com");
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
         driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-
+        sleep(2);
         driver.findElement(By.xpath("//span[normalize-space()='Products']")).click();
+        sleep(2);
         driver.findElement(By.xpath("//span[normalize-space()='Category']")).click();
-        driver.findElement(By.xpath("//input[@id='search']")).sendKeys("sunthuiowi", Keys.ENTER);
+        driver.findElement(By.xpath("//input[@id='search']")).sendKeys("sunthuiowi03", Keys.ENTER);
 
         sleep(2);
-        String name = driver.findElement(By.xpath("//td[normalize-space()='sunthuiowi3']")).getText();
+        String name = driver.findElement(By.xpath("//td[normalize-space()='sunthuiowi03']")).getText();
         System.out.println("Text: " + name);
         sleep(2);
 
